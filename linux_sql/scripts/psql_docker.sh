99799b5c8e09dfca8390216529a1d17a5739f15e
YTY <yty199725@gmail.com>
2019-11-29 19:31:43 +0000

    change

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -5,18 +5,33 @@ operation=$1
 db_password=$2
 export PGPASSWORD="$db_password"
 
-case "$operation" in
-	start)
-		docker run --rm --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
-		psql -h localhost -U postgres
-		;;
-	stop)
-		docker stop jrvs-psql
-		;;
-	*)
-		echo "Usage: ./psql_docker.sh start|stop [dbpassword]"
-		exit 1
-esac
+if [ $operation = "start"]; then
+
+  docker container start jrvs-psql
+  
+  sudo systemctl start docker
+  if docker image ls | grep -q '^postgres'; then
+    echo "postgres image found"
+  else
+    docker pull postgres
+  fi
+  
+  if docker volume ls -f name=pgdata | grep -q 'pgdata'; then
+    echo "docker volume pgdata already exists"
+  else
+    docker volume create pgdata
+  fi
+  docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgresql
+  
+  docker container start jrvs-psql
+  
+  psql -h localhost -U postgres
+  
+else
+  docker container stop jrvs-psql
+  
+  docker container rm jrvs-psql
+fi
 
 exit 0
 	
