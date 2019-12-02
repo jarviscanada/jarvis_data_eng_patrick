6bcf0d8352d88b408d635ea4ef505668fffaef34
YTY <yty199725@gmail.com>
2019-12-02 15:35:22 +0000

    refine

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -3,8 +3,7 @@ SELECT
   cpu_number,
   id as host_id,
   total_mem
-FROM
-  host_info
+FROM host_info
 ORDER BY total_mem DESC;
 
 --Q2
@@ -12,24 +11,16 @@ CREATE TEMP TABLE avgs AS
 SELECT
   host_info.id as host_id,
   host_info.hostname as host_name,
-  (host_info.total_mem * 1.0 - host_usage.memory_free * 1024.0) / host_info.total_mem * 100 AS percentage,
+  (host_info.total_mem - host_usage.memory_free) / host_info.total_mem * 100 AS percentage,
   host_info.total_mem,
-  host_usage.timestamp AS timestamp
-FROM
-  host_info RIGHT OUTER JOIN host_usage ON host_info.id=host_usage.host_id;
+  host_usage.timestamp AS timestamp,
+  date_trunc('hour', host_usage.timestamp) + INTERVAL '5 min' * ROUND( date_part('minute', host_usage.timestamp) / 5.0) AS itvl;
+FROM host_info 
+RIGHT OUTER JOIN host_usage ON host_info.id=host_usage.host_id;
 
-ALTER TABLE avgs ADD COLUMN itvl timestamp;
-UPDATE avgs SET itvl=date_trunc('hour', timestamp) + INTERVAL '5 min' * ROUND( date_part('minute', timestamp) / 5.0);
-
-CREATE TEMP TABLE avg_5min AS
 SELECT 
   host_id, 
   host_name, 
   total_mem, 
   AVG(percentage) over (partition by itvl) AS avg_usage
-FROM 
-  avgs;
-
-SELECT host_id, host_name, total_mem, AVG(avg_usage)
-FROM avg_5min
-GROUP BY host_id, host_name, total_mem;
+FROM avgs;
