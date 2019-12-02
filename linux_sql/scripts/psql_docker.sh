0483127934629a24c662541f1a855aab60c5e5a9
YTY <yty199725@gmail.com>
2019-12-02 02:25:24 +0000

    new implementation

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -5,35 +5,55 @@ operation=$1
 db_password=$2
 export PGPASSWORD="$db_password"
 
-if [ $operation = "start"]; then
+# script usage 
+# ./scripts/psql_docker.sh start|stop [password for database]
 
-  docker container start jrvs-psql
-  
-  sudo systemctl start docker
-  if docker image ls | grep -q '^postgres'; then
-    echo "postgres image found"
-  else
-    docker pull postgres
-  fi
-  
-  if docker volume ls -f name=pgdata | grep -q 'pgdata'; then
-    echo "docker volume pgdata already exists"
-  else
-    docker volume create pgdata
-  fi
-  docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgresql
-  
-  docker container start jrvs-psql
-  
-  psql -h localhost -U postgres
-  
-else
-  docker container stop jrvs-psql
-  
-  docker container rm jrvs-psql
+# check if there is too many arguments
+if (($#>2));
+then
+	echo "WARNING: You have entered too many arguments...
+Format: psql_docker.sh start [password] OR psql_docker.sh stop" >&2
+	exit 1
+fi
+
+# check arguments is start or stop
+if [ "$operation" == "start" ];
+then
+	# start docker_deamon
+	systemctl status docker || systemctl start docker
+	# check if computer is turned on
+	if [[ "$(docker ps -f name=jrvs-psql | wc -l)" == 2 ]];
+	then
+		exit 0
+	fi
+	# check if the volume is created or not
+	if [[ "$(docker volume ls | egrep "pgdata1$" | wc -l)" == 0 ]];
+	then
+		# delete the container named jrvs-psql if there is one
+		if [[ "$(docker ps -a -f name=jrvs-psql | wc -l)" == 2 ]];
+		then
+			docker stop jrvs-psql
+			docker container rm jrvs-psql
+		fi
+		# create the volume and container
+		docker volume create pgdata
+		docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+	# check if the container is created or not
+	elif [[ "$(docker container ls -a | egrep "jrvs-psql$" | wc -l)" == 0 ]];
+	then 
+		docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+	else
+		#start the container
+		docker start jrvs-psql
+		exit 0
+	fi
+# check if it is stop 
+elif [[ "$operation" == "stop" && "$#" == 1 ]];
+then
+	#stop the container & the docker
+	systemctl status docker && docker stop jrvs-psql
+	systemctl status docker && systemctl stop docker
+	exit 0
 fi
 
 exit 0
-	
-
-
