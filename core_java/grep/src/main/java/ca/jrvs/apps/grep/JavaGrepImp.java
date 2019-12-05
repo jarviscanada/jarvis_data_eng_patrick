90cedd10551671db299a3794b1f27a7f388113cc
YTY <yty199725@gmail.com>
2019-12-05 18:48:46 +0000

    build files

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,118 @@
+package ca.jrvs.apps.grep;
+
+import java.io.File;
+import java.io.IOException;
+import java.rmi.server.ExportException;
+import java.util.List;
+
+public class JavaGrepImp implements JavaGrep {
+
+  private String regex;
+  private String rootPath;
+  private String outFile;
+
+  public static void main(String [] args) {
+    if (args.length != 3) {
+      throw new IllegalArgumentException("USAGE: regex rootpath outFile");
+    }
+    JavaGrepImp javaGrepImp = new JavaGrepImp();
+    javaGrepImp.setRegex(args[0]);
+    javaGrepImp.setRootPath(args[1]);
+    javaGrepImp.setOutFile(args[2]);
+
+    try {
+      javaGrepImp.process();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  /**
+   * Top level search workflow
+   *
+   * @throws IOException
+   */
+  @Override
+  public void process() throws IOException {
+
+  }
+
+  /**
+   * Traverse a given directory and return all files
+   *
+   * @param rootDir input directory
+   * @return files under the rootDir
+   */
+  @Override
+  public List<File> listFiles(String rootDir) {
+    return null;
+  }
+
+  /**
+   * Read a file and return all the lies
+   * <p>
+   * Explain FileReader, BufferedReader, and character encoding
+   *
+   * @param inputFile file to be read
+   * @return lines
+   * @throws IllegalArgumentException if a given inputFile is not a file
+   */
+  @Override
+  public List<String> readLines(File inputFile) {
+    return null;
+  }
+
+  /**
+   * check if a line contains the regex pattern (passed by user)
+   *
+   * @param line input string
+   * @return true if there is a match
+   */
+  @Override
+  public boolean containsPattern(String line) {
+    return false;
+  }
+
+  /**
+   * Write lines to a file
+   * <p>
+   * Explore: FileOutputStream, OutputStreamWriter, and BufferedWriter
+   *
+   * @param lines matched line
+   * @throws IOException if write failed
+   */
+  @Override
+  public void writeToFile(List<String> lines) throws IOException {
+
+  }
+
+  @Override
+  public String getRootPath() {
+    return rootPath;
+  }
+
+  @Override
+  public void setRootPath(String rootPath) {
+    this.rootPath = rootPath;
+  }
+
+  @Override
+  public String getRegex() {
+    return regex;
+  }
+
+  @Override
+  public void setRegex(String regex) {
+    this.regex = regex;
+  }
+
+  @Override
+  public String getOutFile() {
+    return outFile;
+  }
+
+  @Override
+  public void setOutFile(String outFile) {
+    this.outFile = outFile;
+  }
+}
