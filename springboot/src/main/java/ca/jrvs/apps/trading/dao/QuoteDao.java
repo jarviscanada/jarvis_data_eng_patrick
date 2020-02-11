638aa861b59f47b6dc0e81e777768942e4f03f4c
YTY <yty199725@gmail.com>
2020-02-11 15:04:45 +0000

    push

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/QuoteDao.java
@@ -0,0 +1,173 @@
+package ca.jrvs.apps.trading.dao;
+
+import ca.jrvs.apps.trading.model.domain.Quote;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class QuoteDao implements CrudRepository<Quote, String> {
+
+  private static final String TABLE_NAME = "quote";
+  private static final String ID_COLUMN_NAME = "ticker";
+
+  private static final Logger logger = LoggerFactory.getLogger(QuoteDao.class);
+  private JdbcTemplate jdbcTemplate;
+  private SimpleJdbcInsert simpleJdbcInsert;
+
+  @Autowired
+  public QuoteDao(DataSource dataSource){
+    jdbcTemplate = new JdbcTemplate(dataSource);
+    simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName(TABLE_NAME);
+  }
+
+  /**
+   * @param quote
+   * @return quote
+   * @throw DataAccessException for unexpected SQL result or SQL execution failure
+   */
+  @Override
+  public Quote save(Quote quote) {
+    if (existsById(quote.getTicker())) {
+      int updateRowNo = updateOne(quote);
+      if(updateRowNo != 1) {
+        throw new DataRetrievalFailureException("Unable to update quote");
+      }
+    }else {
+      addOne(quote);
+    }
+    return quote;
+  }
+
+  /**
+   * helper method that saves one quote
+   */
+  private void addOne(Quote quote) {
+    SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(quote);
+    int row = simpleJdbcInsert.execute(parameterSource);
+    if (row != 1) {
+      throw new IncorrectResultSizeDataAccessException("Failure to insert", 1, row);
+    }
+  }
+
+  /**
+   * helper method that updates one quote
+   */
+  private int updateOne(Quote quote) {
+    String updateSQL = "UPDATE quote SET last_price=?, bid_price=?, bid_size=?, "
+        + "ask_price=?, ask_size=? WHERE ticker=?";
+    return jdbcTemplate.update(updateSQL, makeUpdateValues(quote));
+  }
+
+  /**
+   * helper method that make sql update values objects.
+   * @param quote to be updated
+   * @return UPDATESQL values
+   */
+  private Object[] makeUpdateValues(Quote quote){
+    List<Object> values = new ArrayList<>();
+    values.add(quote.getAskPrice());
+    values.add(quote.getAskSize());
+    values.add(quote.getBidPrice());
+    values.add(quote.getBidSize());
+    values.add(quote.getTicker());
+    values.add(quote.getLastPrice());
+    return values.toArray();
+  }
+
+  @Override
+  public <S extends Quote> Iterable<S> saveAll(Iterable<S> quotes) {
+    List<S> result = new ArrayList<>();
+    for (Quote quote : quotes) {
+      result.add((S) save(quote));
+    }
+    return result;
+  }
+  @Override
+  public Optional<Quote> findById(String ticker) {
+    Optional<Quote> quote = null;
+    String sql = "SELECT * FROM " + TABLE_NAME + " WHERE ticker=? for UPDATE";
+    try {
+      quote = Optional.ofNullable(jdbcTemplate.queryForObject(sql, BeanPropertyRowMapper.newInstance(Quote.class), ticker));
+    }catch (EmptyResultDataAccessException e){
+      logger.debug("Can't find trader id: " + ticker, e);
+    }
+    if (quote == null) {
+      throw new ResourceNotFoundException("Resource not found");
+    }
+    return quote;
+  }
+
+  @Override
+  public boolean existsById(String ticker) {
+    long count;
+    String sql = "SELECT COUNT(*) FROM " + TABLE_NAME +
+        " WHERE ticker='" + ticker.toUpperCase() + "'";
+    try {
+      count = jdbcTemplate.queryForObject(sql, Long.class);
+    } catch (NullPointerException e){
+      count = 0;
+    }
+    return count != 0;
+  }
+
+  @Override
+  public List<Quote> findAll() {
+    String sql = "SELECR * FROM " + TABLE_NAME;
+    return jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Quote.class));
+  }
+
+  @Override
+  public Iterable<Quote> findAllById(Iterable<String> iterable) {
+    throw new UnsupportedOperationException("Not Implemented.");
+  }
+
+  @Override
+  public long count() {
+    long count;
+    String sql = "SELECT COUNT(*) FROM " + TABLE_NAME;
+    try {
+      count = jdbcTemplate.queryForObject(sql, Long.class);
+    } catch (NullPointerException e){
+      count = 0;
+    }
+    return count;
+  }
+
+  @Override
+  public void deleteById(String ticker) {
+    Object args = new Object[] {ticker};
+    String sql = "DELETE FROM " + TABLE_NAME + " WHERE ticker=?";
+    jdbcTemplate.update(sql, ticker);
+  }
+
+  @Override
+  public void delete(Quote quote) {
+    throw new UnsupportedOperationException("Not Implemented.");
+  }
+
+  @Override
+  public void deleteAll(Iterable<? extends Quote> iterable) {
+    throw new UnsupportedOperationException("Not Implemented.");
+  }
+
+  @Override
+  public void deleteAll() {
+    String sql = "DELET FROM " + TABLE_NAME;
+    jdbcTemplate.update(sql);
+  }
+}
--- springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
@@ -1,7 +1,12 @@
 package ca.jrvs.apps.trading.service;
 
 import ca.jrvs.apps.trading.dao.MarketDataDao;
+import ca.jrvs.apps.trading.dao.QuoteDao;
 import ca.jrvs.apps.trading.model.domain.IexQuote;
+import ca.jrvs.apps.trading.model.domain.Quote;
+import com.sun.org.apache.xpath.internal.operations.Quo;
+import java.util.ArrayList;
+import java.util.List;
 import javax.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +19,12 @@ public class QuoteService {
 
   private static final Logger logger = LoggerFactory.getLogger(QuoteService.class);
 
-  private IexQuote iexQuote;
+  private QuoteDao quoteDao;
   private MarketDataDao marketDataDao;
 
   @Autowired
-  public QuoteService(IexQuote iexQuote, MarketDataDao marketDataDao){
-    this.iexQuote = iexQuote;
+  public QuoteService(QuoteDao quoteDao, MarketDataDao marketDataDao){
+    this.quoteDao = quoteDao;
     this.marketDataDao = marketDataDao;
   }
 
@@ -32,5 +37,54 @@ public class QuoteService {
    */
   public IexQuote findIexQuoteByTicker(String ticker){
     return marketDataDao.findById(ticker).orElseThrow(() -> new IllegalArgumentException(ticker + " is invalid"));
+  }
+
+  /**
+   * Helper method, Map an IexQuote object to a Quote object.
+   *
+   * @param iexQuote
+   * @return
+   */
+  protected static Quote buildQuoteFromIexQuote(IexQuote iexQuote){
+    
+  }
+
+  /**
+   * Update quote table against IEX source
+   *  - get all quotes from the db
+   *  - foreach ticker get IexQuote
+   *  - convert iexQuote to quote entity
+   *  - persist quote to db
+   *
+   * @throws ca.jrvs.apps.trading.dao.ResourceNotFoundException if ticker is not found from IEX
+   * @throws org.springframework.dao.DataAccessException if unable to retrieve data
+   * @throws IllegalArgumentException for invalid input
+   */
+  public void updateMarketData(){
+    List<Quote> quotes = quoteDao.findAll();
+    IexQuote iexQuote;
+    Quote quoteTemp;
+    for (Quote quote : quotes){
+      String ticker = quote.getTicker();
+      iexQuote = marketDataDao.findById(ticker).get();
+      quoteTemp = buildQuoteFromIexQuote(iexQuote);
+      quoteDao.save(quoteTemp);
+    }
+  }
+
+  /**
+   * Helper method: Map a IexQuote to a Quote entity
+   * Note: `iexQuote.getLastestPrice() == null` if the stock market is closed.
+   * Make sure set a default value for number fields.
+   */
+  protected static Quote buildQuoteFromIexQuote(IexQuote iexQuote){
+    Quote quote = new Quote();
+    quote.setTicker((String) iexQuote.getSymbol());
+    quote.setLastPrice(iexQuote.getLatestPrice());
+    quote.setAskPrice(iexQuote.getIexAskPrice());
+    quote.setAskSize(iexQuote.getIexAskSize());
+    quote.setBidPrice(iexQuote.getIexBidPrice());
+    quote.setBidSize(iexQuote.getIexBidSize());
+    return quote;
   }
 }
