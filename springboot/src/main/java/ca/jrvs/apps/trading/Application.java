17161f8048edff1443405c5d74637d91e74f5bee
YTY <yty199725@gmail.com>
2020-03-09 05:41:37 +0000

    readme

--- springboot/src/main/java/ca/jrvs/apps/trading/Application.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/Application.java
@@ -4,17 +4,18 @@ import ca.jrvs.apps.trading.service.QuoteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication(exclude = {JdbcTemplateAutoConfiguration.class,
-    DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+    DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+@Component
 public class Application implements CommandLineRunner {
 
   private Logger logger = LoggerFactory.getLogger(Application.class);
@@ -22,13 +23,13 @@ public class Application implements CommandLineRunner {
   @Autowired
   private QuoteService quoteService;
 
-  public static void main(String[] args) throws Exception{
+  public static void main (String[] args) throws Exception{
     SpringApplication app = new SpringApplication(Application.class);
     app.run(args);
   }
 
   @Override
-  public void run(String... args) throws Exception {
+  public void run(String... args) throws Exception{
 
   }
 }
